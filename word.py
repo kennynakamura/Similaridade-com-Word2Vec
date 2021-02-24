960729574f8421b174131abcafec55d650cac0c5
Kenny <kennynakamura@hotmail.com>
2021-02-24 09:47:29 -0300

    Update word.py

--- word.py
+++ word.py
@@ -2,30 +2,21 @@ import gensim
 import os
 import nltk.data
 import string
-from nltk.corpus import stopwords
 import itertools
-nltk.download('stopwords')
 
 model = gensim.models.Word2Vec.load('C:/Users/kenny.ksn/Desktop/vocabularynovo.model')
 
-with open('C:/Users/kenny.ksn/Desktop/Word2Vec/Texto4.txt', 'r', encoding = 'utf-8-sig') as file:
+with open('PATH/Texto.txt', 'r', encoding = 'utf-8-sig') as file:
       data = file.read().lower()
       data = data.split('\n')
       data = [x for x in data if x] 
 
 lista = []
 lista2 = []
-lista3 = []
+
 for item in data:
    ff = item.split()
    lista.append(ff)
-'''
-for item in lista:
-   filtered_words = [word for word in item if word not in stopwords.words('portuguese')]
-   lista2.append(filtered_words)
-
-print(lista2)
-'''
 
 print(lista)
 for item in lista:
@@ -34,7 +25,7 @@ for item in lista:
       valor = model.similarity(a,b)
       print(valor, a, b)
       if valor < 0.2:
-         lista3.append(a)
-         lista3.append(b)
+         lista2.append(a)
+         lista2.append(b)
       
-print(lista3)
+print(lista2)
